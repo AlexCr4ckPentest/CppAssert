8016b01a7aebacccb7a818d0fcb84cebe8b7f7e2
AlexCr4ckPentest <48525682+AlexCr4ckPentest@users.noreply.github.com>
2021-01-06 00:22:58 +0300

    remove if-constexpr (because non-constant value)

--- cpp_assert.hpp
+++ cpp_assert.hpp
@@ -22,14 +22,15 @@ void __default_assertion_handler(const char* filename, int line, const char* fun
 
 
 
-template<bool expr, typename Handler,
+template<Typename Expression, typename Handler,
          typename = std::enable_if_t<
+             std::is_convertible_v<Expression, bool> &&
              std::is_invocable_r_v<void, Handler, const char*, int, const char*, const char*, std::ostream&>
              >
 >
 constexpr void __assert_base(const char* filename, int line, Handler&& handler) noexcept
 {
-    if constexpr (!std::bool_constant<expr>::value)
+    if (!expr)
     {
         handler(filename, line);
         std::terminate();
@@ -41,11 +42,11 @@ constexpr void __assert_base(const char* filename, int line, Handler&& handler) 
 
 
 #define ASSERT_DEFAULT(expression)  \
-    alex::assert::__assert_base<static_cast<bool>(expression)>(__FILE__, __LINE__,    \
+    alex::assert::__assert_base(expression, __FILE__, __LINE__,    \
                                 std::bind(alex::assert::__default_assertion_handler, std::placeholders::_1, std::placeholders::_2, __PRETTY_FUNCTION__, #expression, std::ref(std::cerr)))
 
 #define ASSERT_CUSTOM(expression, handler, stream)  \
-    alex::assert::__assert_base<static_cast<bool>(expression)>(__FILE__, __LINE__,    \
+    alex::assert::__assert_base(expression, __FILE__, __LINE__,    \
                                 std::bind(handler, std::placeholders::_1, std::placeholders::_2, __PRETTY_FUNCTION__, #expression, std::ref(stream)))
 
 #endif // CPP_ASSERT_HPP
